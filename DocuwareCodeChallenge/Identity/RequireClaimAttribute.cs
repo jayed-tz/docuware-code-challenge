b795fdba2c6257e09129ca82af40785cf17ab1f9
Jayed Talukder <jayed.tz@gmail.com>
2024-03-17 11:56:08 +0100

    chore: Added class

---
+++ DocuwareCodeChallenge/Identity/RequireClaimAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace DocuwareCodeChallenge.Identity
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class RequireClaimAttribute : Attribute, IAuthorizationFilter
+    {
+        private readonly string _claimName;
+        private readonly string _claimValue;
+        public RequireClaimAttribute(string claimName, string claimValue)
+        {
+            _claimName = claimName;
+            _claimValue = claimValue;
+        }
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            if (!context.HttpContext.User.HasClaim(_claimName, _claimValue))
+            {
+                context.Result = new ForbidResult();
+            }
+        }
+    }
+}
+
